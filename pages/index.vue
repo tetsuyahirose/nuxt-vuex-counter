acffc8c0057014aa61a9571eab3fd86e164e6470
tetsuyahirose <tetsuya19990510gmail.com>
2020-10-07 14:49:11 +0900

    カウンター機能つけました

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <div>
+      <Counter></Counter>
+      <hr />
+      <Counter></Counter>
+      <hr />
+      <Counter></Counter>
+      <hr />
+    </div>
+  </section>
+</template>
+
+<script>
+import Counter from "~/components/Counter.vue";
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
